1a61b866ae3e5a4e9152505a12ce06370226bf64
due-nguyen <rb.dueng@gmail.com>
2021-06-21 21:51:15 -0700

    counter files are added

---
+++ counter.js
@@ -0,0 +1,11 @@
+let counter = 0;
+function count() {
+	counter = counter + 1;
+	document.querySelector('h1').innerHTML = counter;
+
+	if (counter % 10 === 0) {
+		alert(`The count is now ${counter}`);
+	}
+}
+
+document.querySelector('button').onclick = count;
